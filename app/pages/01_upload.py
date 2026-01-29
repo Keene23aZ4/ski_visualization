a562ef0dacee14e94690a9b2c673129fcf2ea691
Keene23aZ4 <kentoyamaza17@gmail.com>
2026-01-29 19:23:24 +0900

    Update 01_upload.py

--- app/pages/01_upload.py
+++ app/pages/01_upload.py
@@ -1,14 +1,11 @@
 import sys
 from pathlib import Path
-
 ROOT = Path(__file__).resolve().parents[2]
 sys.path.append(str(ROOT))
 
 import streamlit as st
 import uuid
 from app.config import UPLOAD_DIR
-from core.pose.run_pose import run_mediapipe
-from core.pose.mediapipe_adapter import extract_frames_from_mediapipe
 
 st.header("① 動画アップロード")
 
@@ -23,9 +20,3 @@ if video:
 
     st.success("アップロード完了")
     st.session_state["video_path"] = save_path
-    
-if "video_path" in st.session_state:
-    if st.button("② 解析開始"):
-        results_list = run_mediapipe(st.session_state["video_path"])
-
-        st.write("frames from mediapipe:", len(results_list))
